818245a3135d73819b9d4a74ba59236e0f633f2f
cesarVizentini <cesar.vizentini@gmail.com>
2021-11-05 21:56:27 -0300

    implementacao do ex21 trabalhando com jdbc

---
+++ src/ex21/PetControl.java
@@ -0,0 +1,150 @@
+package ex21;
+
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class PetControl {
+
+    private static final String DBURL = "jdbc:mariadb://localhost:3306/petdb";
+    private static final String DBUSER = "root";
+    private static final String DBPASS = "123456";
+
+    LongProperty id = new SimpleLongProperty(0);
+    StringProperty nome = new SimpleStringProperty("");
+    StringProperty raca = new SimpleStringProperty("");
+    DoubleProperty peso = new SimpleDoubleProperty(0);
+    ObjectProperty nascimento = new SimpleObjectProperty(LocalDate.now());
+
+    private static long counter = 0;
+
+    private List<Pet> lista = new ArrayList<>();
+    private ObservableList<Pet> listaView = FXCollections.observableArrayList();
+
+    public Pet getEntity() {
+        Pet p = new Pet();
+        p.setId(id.get());
+        p.setNome(nome.get());
+        p.setRaca(raca.get());
+        p.setPeso(peso.get());
+        p.setNascimento((LocalDate)nascimento.get());
+        return p;
+    }
+
+    public void setEntity(Pet p) {
+        id.set(p.getId());
+        nome.set(p.getNome());
+        raca.set(p.getRaca());
+        peso.set(p.getPeso());
+        nascimento.set(p.getNascimento());
+    }
+
+    public void salvar() {
+        Pet p = getEntity();
+//        boolean encontrado = false;
+//        for (int i = 0; i < lista.size(); i++) {
+//            Pet pet = lista.get(i);
+//            if (p.getId() == pet.getId()) {
+//                lista.set(i, p);
+//                encontrado = true;
+//                break;
+//            }
+//        }
+//
+//        if (!encontrado) {
+//            lista.add(p);
+//        }
+
+        try {
+            Connection con = DriverManager.getConnection(DBURL, DBUSER, DBPASS);
+
+            String sql = String.format(Locale.ROOT,
+                    "INSERT INTO pet (id, nome, raca, peso, nascimento)  " +
+                            "VALUES (%d, '%s', '%s', %f, '%s')",
+                    p.getId(),
+                    p.getNome(),
+                    p.getRaca(),
+                    p.getPeso(),
+                    p.getNascimento()
+            );
+
+            System.out.println(sql);
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.executeUpdate();
+
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        atualizarListaView();
+    }
+
+    public void novoPet() {
+        Pet p = new Pet();
+        p.setId(++counter);
+        setEntity(p);
+    }
+
+    public void pesquisar() {
+        listaView.clear();
+//        for(Pet p : lista) {
+//            if (p.getNome().contains(nome.get())) {
+//                listaView.add(p);
+//               // setEntity(p);
+//               // break;
+//            }
+//        }
+        try {
+            Connection con = DriverManager.getConnection(DBURL, DBUSER, DBPASS);
+
+            String sql = "SELECT * FROM pet WHERE nome like '%" + nome.get() + "%'";
+            System.out.println(sql);
+
+            PreparedStatement stmt = con.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()) {
+
+                Pet p = new Pet();
+                p.setId( rs.getLong("id") );
+                p.setNome( rs.getString("nome") );
+                p.setRaca( rs.getString("raca") );
+                p.setPeso( rs.getDouble("peso") );
+                p.setNascimento( rs.getDate("nascimento").toLocalDate() );
+                listaView.add(p);
+            }
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void remover(long id) {
+        for (Pet p : lista) {
+            if (p.getId() == id) {
+                lista.remove(p);
+                break;
+            }
+        }
+        atualizarListaView();
+    }
+
+    public void atualizarListaView() {
+        listaView.clear();
+        listaView.addAll(lista);
+    }
+
+    public ObservableList<Pet> getListaView() {
+        return listaView;
+    }
+}
---
+++ src/ex21/TesteConexao.java
@@ -0,0 +1,40 @@
+package ex21;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class TesteConexao {
+    private static final String DBURL = "jdbc:mariadb://localhost:3308/poo";
+    private static final String DBUSER = "root";
+    private static final String DBPASS= "258460";
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Teste de conexão em DB");
+        Class.forName("org.mariadb.jdbc.Driver");
+        System.out.println("Classe de conexao carregada");
+        Connection con = DriverManager.getConnection(DBURL, DBUSER, DBPASS);
+        System.out.println("Conectado no banco de dados");
+
+//        String sql = "INSERT INTO pet (id, nome, raca, peso, nascimento) " +
+//                "VALUES (2, 'Caramelo', 'ViraLata', 10.76, '2017-11-09')";
+//
+//        PreparedStatement stmt = con.prepareStatement(sql);
+//        stmt.executeUpdate();
+
+        String sql = "SELECT * FROM pet";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery();
+
+        while(rs.next()) {
+            System.out.println(rs.getString("nome"));
+            System.out.println(rs.getString("raca"));
+            System.out.println(rs.getDouble("peso"));
+            System.out.println(rs.getDate("nascimento"));
+            System.out.println("--------------------------------------");
+        }
+
+        con.close();
+    }
+}
